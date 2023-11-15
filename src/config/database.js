c66409b726f122aa554ad9b654d3d207caace46a
tringuyen1 <46176254+tringuyen1@users.noreply.github.com>
2023-11-15 17:22:14 +0700

    connection db

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+// test connection 
+const connection = mysql.createConnection({
+     host: `${process.env.DB_HOST}`,
+     user: `${process.env.DB_USER}`,
+     database: `${process.env.DB_NAME}`,
+     port: `${process.env.DB_PORT}`,
+     password: `${process.env.DB_PASSWORD}`
+});
+
+module.exports = {
+     connection
+}
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express() // app express
 const configViewEngine = require("./config/viewEngine")
 const webRoutes = require("./routes/web")
+const { connection } = require('./config/database')
 
 
 const port = process.env.PORT || 8001; // port
@@ -13,6 +14,14 @@ configViewEngine(app);
 
 // khai báo route
 app.use("/v1", webRoutes)
+
+connection.query(
+     'SELECT * FROM Users',
+     ['Page', 45],
+     function (err, results, fields) {
+          console.log("check results >>>>", results);
+     }
+);
 
 app.listen(port, hostName, () => {
      console.log(`Example app listening on port ${port}`) // cho chạy server
